533badb1cde6b801b53e1e263ad809b35947045f
Eric Chang <eric.chang.1015@gmail.com>
2017-09-04 08:46:13 +0800

    Add files via upload

--- get18avVideo.sh
+++ get18avVideo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#todo download album
+#todo save url serial as path
+#todo downloading filename should be xxx.downloading
+#todo curl continue download when terminate => curl -C
+#todo only two files are downloading at the same time
+#todo daily download latest video
+
+
 set -e
 
 URL=$1
@@ -25,27 +33,28 @@ echo $EmbedVideoWenList
 declare -i index=0
 for embedVideoWeb in $EmbedVideoWenList; do
 	index+=1
-	echo $embedVideoWeb
+	filename="$videoTitle $index.mp4"
+	videoFile=$downloadFolder/$filename
+	echo filename=$filename
+	echo embedVideoWeb=$embedVideoWeb
 	embedMetafileName=$(echo $embedVideoWeb | sed "s/.*\///g" )
 	embedMetafile=$metadataFolder/$embedMetafileName
-	if [ ! -f $embedMetafile ]; then
+	if [ ! -f "$videoFile" ]; then
 		cd $metadataFolder
 		curl $embedVideoWeb -o $embedMetafileName
 		cd -
 	else
 		echo "already download"
 		continue
-		echo "fail"
 	fi
 	videoUrl=https:$(grep mp4 $embedMetafile | sed "s/\,/\n/g" | grep filename | grep -v m3u8 | sed "s/\"/\n/g" | grep mp4 | sed "s/\\\//g")
 	echo $videoUrl
-	cd $downloadFolder
-	filename="$videoTitle $index.mp4"
-	echo $filename
-	if [ ! -f $filename ]; then
+
+	if [ ! -f "$videoFile" ]; then
+		cd $downloadFolder
 		curl $videoUrl -o "$filename" &
+		cd -
 	else
 		echo "already download $filename"
 	fi 
-	cd -
 done
