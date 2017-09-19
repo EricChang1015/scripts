59156abea67c4ee3b06d57489a63fb99911d9c84
EricChang1015 <eric.chang.1015@gmail.com>
2017-09-19 18:09:52 +0800

    1. replace tab with space 2. remove redundant code

--- get18avVideo.sh
+++ get18avVideo.sh
@@ -5,6 +5,7 @@
 # feature:
 # 09/17: add download list and check if force download or not.
 # 09/18: curl continue download when terminate => curl -C (test fail)
+# 09/19: use proxy for AWS can access this web
 
 #set -e
 
@@ -34,18 +35,18 @@ metadataFolder=$downloadTo/metadata/video
 
 main()
 {
-	if [ $# -eq 0 ]; then
-		show_help
-		return
-	fi
-	parseParameters $@
-	for source in $@; do
-		execute preSetting $source       || continue
-		execute getWebInfo               || return $?
-		execute getVideoTitle            || return $?
-		execute downloadPreviewImages    || return $?
-		execute downloadVideo            || return $?
-	done
+    if [ $# -eq 0 ]; then
+        show_help
+        return
+    fi
+    parseParameters $@
+    for source in $@; do
+        execute preSetting $source       || continue
+        execute getWebInfo               || return $?
+        execute getVideoTitle            || return $?
+        execute downloadPreviewImages    || return $?
+        execute downloadVideo            || return $?
+    done
 }
 
 function execute()
@@ -65,210 +66,203 @@ function execute()
 #Note: use ":"and "$OPTARG" to argument
 function parseParameters()
 {
-	echo $@
-	while getopts "h?vpfpv?h:" opt; do
-	echo $opt
-		case "$opt" in
-		h|\?)
-			show_help
-			exit 0
-			;;
-		v)
-			set -x
-			verbose='y'
-			echo -e ${C_YELLOW}"verbose"${C_RESET}
-			;;
-		f)
-			forceDownload='y'
-			echo -e ${C_YELLOW}"force"${C_RESET}
-			;;
-		p)
-			proxy='y'
-			echo -e ${C_YELLOW}"use proxy $proxy_server"${C_RESET}
-		esac
-	done
+    echo $@
+    while getopts "h?vpfpv?h:" opt; do
+    echo $opt
+        case "$opt" in
+        h|\?)
+            show_help
+            exit 0
+            ;;
+        v)
+            set -x
+            verbose='y'
+            echo -e ${C_YELLOW}"verbose"${C_RESET}
+            ;;
+        f)
+            forceDownload='y'
+            echo -e ${C_YELLOW}"force"${C_RESET}
+            ;;
+        p)
+            proxy='y'
+            echo -e ${C_YELLOW}"use proxy $proxy_server"${C_RESET}
+        esac
+    done
 }
 
 function show_help()
 {
-	echo -e "${C_YELLOW}=============<<<HELP>>>=================${C_RESET}"
-	echo -e "${C_YELLOW}This bash script help you to download   ${C_RESET}"
-	echo -e "${C_YELLOW}video from http://18av.mm-cg.com        ${C_RESET}"
-	echo -e "${C_YELLOW}========================================${C_RESET}"
-	echo -e "=            How to use                ="
-	echo -e "-f force download, even in $downloadList"
-	echo -e "----------------------------------------"
-	echo -e "Exp 1. Download with whole URL"
-	echo -e "${C_YELLOW}$0 http://18av.mm-cg.com/18av/23623.html ...${C_RESET}"
-	echo -e "----------------------------------------"
-	echo -e "Exp 2. Download with reference video number"
-	echo -e "${C_YELLOW}$0 23623 26437 ...${C_RESET}"
-	echo -e "========================================"
+    echo -e "${C_YELLOW}=============<<<HELP>>>=================${C_RESET}"
+    echo -e "${C_YELLOW}This bash script help you to download   ${C_RESET}"
+    echo -e "${C_YELLOW}video from http://18av.mm-cg.com        ${C_RESET}"
+    echo -e "${C_YELLOW}========================================${C_RESET}"
+    echo -e "=            How to use                ="
+    echo -e "-f force download, even in $downloadList"
+    echo -e "----------------------------------------"
+    echo -e "Exp 1. Download with whole URL"
+    echo -e "${C_YELLOW}$0 http://18av.mm-cg.com/18av/23623.html ...${C_RESET}"
+    echo -e "----------------------------------------"
+    echo -e "Exp 2. Download with reference video number"
+    echo -e "${C_YELLOW}$0 23623 26437 ...${C_RESET}"
+    echo -e "========================================"
 }
 
 function preSetting()
 {
-	source=$1
-	testInput="$(echo $source | grep html)"
-	if [ ! -z $testInput ]; then
-		URL=$1
-	elif [[ $source =~ ^-?[0-9]+$ ]] ;then
-		URL="http://18av.mm-cg.com/18av/${source}.html"
-	else
-		echo "invalid source $source"
-		return $ERROR_INVALID_SOURCE
-	fi
+    source=$1
+    testInput="$(echo $source | grep html)"
+    if [ ! -z $testInput ]; then
+        URL=$1
+    elif [[ $source =~ ^-?[0-9]+$ ]] ;then
+        URL="http://18av.mm-cg.com/18av/${source}.html"
+    else
+        echo "invalid source $source"
+        return $ERROR_INVALID_SOURCE
+    fi
 
-	htmlFileName=$(echo $URL | sed "s/.*\///g" )
-	subFolder=$(echo $htmlFileName | sed "s/.html//g" )
-	downloadFolder=$downloadTo/$subFolder
-	
-	cat $downloadListPattern | awk '{print $1}' | grep $subFolder
-	# already download
-	if [ $? -eq 0 ]; then
-		if [ "$forceDownload" == 'n' ] && [ ! -d $downloadFolder ]; then
-			echo "already download $(cat $downloadListPattern | grep $subFolder)"
-			return $ERROR_ALREADY_DOWNLOAD
-		fi
-	fi
+    htmlFileName=$(echo $URL | sed "s/.*\///g" )
+    subFolder=$(echo $htmlFileName | sed "s/.html//g" )
+    downloadFolder=$downloadTo/$subFolder
 
-	htmlFile=$metadataFolder/$htmlFileName
-	mkdir -p $metadataFolder
-	mkdir -p $downloadFolder
+    cat $downloadListPattern | awk '{print $1}' | grep $subFolder
+    # already download
+    if [ $? -eq 0 ]; then
+        if [ "$forceDownload" == 'n' ] && [ ! -d $downloadFolder ]; then
+            echo "already download $(cat $downloadListPattern | grep $subFolder)"
+            return $ERROR_ALREADY_DOWNLOAD
+        fi
+    fi
+
+    htmlFile=$metadataFolder/$htmlFileName
+    mkdir -p $metadataFolder
+    mkdir -p $downloadFolder
 }
 
 function testHtml(){
-	grep youjizz $htmlFileName > /dev/null
+    grep youjizz $htmlFileName > /dev/null
 }
 
 function myCurl(){
-	if [ "$proxy" == "n" ]; then
-		curl $@
-	else
-		curl -x $proxy_server $@
-	fi
+    if [ "$proxy" == "n" ]; then
+        curl $@
+    else
+        curl -x $proxy_server $@
+    fi
 }
 
 function tryProxy(){
-	echo "try $1 proxy"
-	proxy="$1"
-	myCurl $URL -o $htmlFileName
-	testHtml
-	if [ $? -eq 0 ]; then
-		echo edit setting
-		cd -  > /dev/null
-		sed "s/^proxy=.*/proxy=\'$1\'/g" -i $0
-		cd -  > /dev/null
-	fi
+    echo "try $1 proxy"
+    proxy="$1"
+    myCurl $URL -o $htmlFileName
+    testHtml
+    if [ $? -eq 0 ]; then
+        echo edit setting
+        cd -  > /dev/null
+        sed "s/^proxy=.*/proxy=\'$1\'/g" -i $0
+        cd -  > /dev/null
+    fi
 }
 
 function getWebInfo(){
-	error_code=$ERROR_NONE
-	if [ ! -f $htmlFile ]; then
-		cd $metadataFolder
+    error_code=$ERROR_NONE
+    if [ ! -f $htmlFile ]; then
+        cd $metadataFolder
 
-		myCurl $URL -o $htmlFileName
-		#test if content usefull info
-		testHtml
-		if [ ! $? -eq 0 ]; then
-			if [ "$proxy" == "n" ]; then
-				tryProxy y
-			else
-				tryProxy n
-			fi
-		fi
-		testHtml
-		if [ ! $? -eq 0 ]; then
-			error_code=$ERROR_INVALID_HTML
-		fi
-		cd - > /dev/null
-	fi
-	return $error_code
+        myCurl $URL -o $htmlFileName
+        #test if content usefull info
+        testHtml
+        if [ ! $? -eq 0 ]; then
+            if [ "$proxy" == "n" ]; then
+                tryProxy y
+            else
+                tryProxy n
+            fi
+        fi
+        testHtml
+        if [ ! $? -eq 0 ]; then
+            error_code=$ERROR_INVALID_HTML
+        fi
+        cd - > /dev/null
+    fi
+    return $error_code
 }
 
 function getVideoTitle()
 {
-	videoTitle=$(cat $htmlFile | grep "影片名稱" | sed "s/<br>/\n/g" | grep "影片名稱" | sed "s/.*>//g" | sed "s/,/ /g")
-	echo "$subFolder , $videoTitle" > $downloadtemp
-	cat $downloadListPattern >> $downloadtemp
-	rm -f $downloadListPattern
-	cat $downloadtemp | sort -u > $downloadList
-	title=$videoTitle
-	if [ -z "$title" ]; then
-		rm -f $htmlFile
-		return $ERROR_INVALID_TITLE
-	fi
+    videoTitle=$(cat $htmlFile | grep "影片名稱" | sed "s/<br>/\n/g" | grep "影片名稱" | sed "s/.*>//g" | sed "s/,/ /g")
+    echo "$subFolder , $videoTitle" > $downloadtemp
+    cat $downloadListPattern >> $downloadtemp
+    rm -f $downloadListPattern
+    cat $downloadtemp | sort -u > $downloadList
+    title=$videoTitle
+    if [ -z "$title" ]; then
+        rm -f $htmlFile
+        return $ERROR_INVALID_TITLE
+    fi
 }
 
 
 
 function downloadPreviewImages()
 {
-	videoPreviewImages=$(cat $htmlFile | grep "影片名稱" | sed "s/<br>/\n/g"  | sed "s/http:/\nhttp:/g" | sed "s/jpg.*/jpg/g" | grep http | grep jpg | xargs)
-	for ImageUrl in $videoPreviewImages; do
-		filename=$(echo $ImageUrl | sed "s/.*\///g" )
-		ImageFile=$downloadFolder/$filename
-		if [ ! -f "$ImageFile" ]; then
-			cd $downloadFolder
-			curl -O $ImageUrl > /dev/null 2>&1 &
-			sleep 0.5
-			cd - > /dev/null
-		fi 
-	done
+    videoPreviewImages=$(cat $htmlFile | grep "影片名稱" | sed "s/<br>/\n/g"  | sed "s/http:/\nhttp:/g" | sed "s/jpg.*/jpg/g" | grep http | grep jpg | xargs)
+    for ImageUrl in $videoPreviewImages; do
+        filename=$(echo $ImageUrl | sed "s/.*\///g" )
+        ImageFile=$downloadFolder/$filename
+        if [ ! -f "$ImageFile" ]; then
+            cd $downloadFolder
+            curl -O $ImageUrl > /dev/null 2>&1 &
+            sleep 0.5
+            cd - > /dev/null
+        fi
+    done
 }
 
 function downloadVideo()
 {
-	EmbedVideoWenList=$(cat $htmlFile | grep embed | sed "s/http/\nhttp/g" | sed "s/\".*//g" | grep embed | grep youjizz | xargs)
-	echo $EmbedVideoWenList
-	declare -i index=0
-	for embedVideoWeb in $EmbedVideoWenList; do
-		index+=1
-		filename="$videoTitle $index.mp4"
-		filename_downloading="$videoTitle $index.mp4.download"
-		videoFile=$downloadFolder/$filename
-		videoFile_downloading=$downloadFolder/$filename_downloading
-		echo filename=$filename
-		echo embedVideoWeb=$embedVideoWeb
-		embedMetafileName=$(echo $embedVideoWeb | sed "s/.*\///g" )
-		embedMetafile=$metadataFolder/$embedMetafileName
-		if [ ! -f "$videoFile" ]; then
-			cd $metadataFolder
-			curl $embedVideoWeb -o $embedMetafileName
-			cd - > /dev/null
-		else
-			echo "already download $videoFile"
-			continue
-		fi
-		videoHD=$(grep mp4 $embedMetafile | sed "s/\,/\n/g" | grep filename | grep -v m3u8 | sed "s/\"/\n/g" | grep mp4 | sed "s/\\\//g" | grep -E "\-1080\-|\-720\-" | head -1)
-		videoSD=$(grep mp4 $embedMetafile | sed "s/\,/\n/g" | grep filename | grep -v m3u8 | sed "s/\"/\n/g" | grep mp4 | sed "s/\\\//g" | grep -E "\-480\-|\-426\-|\-320\-|\-360\-" | head -1)
-		if [ ! -z $videoHD ]; then
-			videoUrl=https:$videoHD
-		elif [ ! -z $videoSD ]; then
-			videoUrl=https:$videoSD
-		else
-			echo "cann't resolve videoUrl of $filename"
-			continue
-		fi
-		echo $videoUrl
+    EmbedVideoWenList=$(cat $htmlFile | grep embed | sed "s/http/\nhttp/g" | sed "s/\".*//g" | grep embed | grep youjizz | xargs)
+    echo $EmbedVideoWenList
+    declare -i index=0
+    for embedVideoWeb in $EmbedVideoWenList; do
+        index+=1
+        filename="$videoTitle $index.mp4"
+        filename_downloading="$videoTitle $index.mp4.download"
+        videoFile=$downloadFolder/$filename
+        videoFile_downloading=$downloadFolder/$filename_downloading
+        echo filename=$filename
+        echo embedVideoWeb=$embedVideoWeb
+        embedMetafileName=$(echo $embedVideoWeb | sed "s/.*\///g" )
+        embedMetafile=$metadataFolder/$embedMetafileName
+        if [ ! -f "$videoFile" ]; then
+            cd $metadataFolder
+            curl $embedVideoWeb -o $embedMetafileName
+            cd - > /dev/null
+        else
+            echo "already download $videoFile"
+            continue
+        fi
+        videoHD=$(grep mp4 $embedMetafile | sed "s/\,/\n/g" | grep filename | grep -v m3u8 | sed "s/\"/\n/g" | grep mp4 | sed "s/\\\//g" | grep -E "\-1080\-|\-720\-" | head -1)
+        videoSD=$(grep mp4 $embedMetafile | sed "s/\,/\n/g" | grep filename | grep -v m3u8 | sed "s/\"/\n/g" | grep mp4 | sed "s/\\\//g" | grep -E "\-480\-|\-426\-|\-320\-|\-360\-" | head -1)
+        if [ ! -z $videoHD ]; then
+            videoUrl=https:$videoHD
+        elif [ ! -z $videoSD ]; then
+            videoUrl=https:$videoSD
+        else
+            echo "cann't resolve videoUrl of $filename"
+            continue
+        fi
+        echo $videoUrl
 
-		echo "$videoFile_downloading"
-		if [ -f "$videoFile_downloading" ] ; then
-			echo "continue download $videoFile_downloading"
-			offsetBytes=$(ls "$videoFile_downloading" -l | awk '{print $5}')
-			cd $downloadFolder
-			curl -L -C - $videoUrl -o "$filename_downloading"
-			mv "$filename_downloading" "$filename"
-			cd - > /dev/null
-		elif [ ! -f "$videoFile" ]; then
-			cd $downloadFolder
-			curl -L -C - $videoUrl -o "$filename_downloading"
-			mv "$filename_downloading" "$filename"
-			cd - > /dev/null
-		else
-			echo "already download $filename"
-		fi 
-	done
+        echo "$videoFile_downloading"
+        if [ -f "$videoFile_downloading" ] || [ ! -f "$videoFile" ] ; then
+            cd $downloadFolder
+            curl -L -C - $videoUrl -o "$filename_downloading"
+            mv "$filename_downloading" "$filename"
+            cd - > /dev/null
+        else
+            echo "already download $filename"
+        fi
+    done
 }
 
 main $@
